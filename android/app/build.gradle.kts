f7b39a0088b32c4f276a660b0c1c2d5a900595f7
0x7f-core <104748634+0x7f-core@users.noreply.github.com>
2026-09-01 16:10:28 +0900

    Bump Android app to v4.15

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agitq.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "4.14"
+        versionCode = 16
+        versionName = "4.15"
     }
 
     signingConfigs {
