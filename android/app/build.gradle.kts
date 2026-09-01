4c91391a99869dea160ca275c3c504892cf40214
0x7f-core <104748634+0x7f-core@users.noreply.github.com>
2026-09-01 15:58:44 +0900

    Bump Android app to v4.14

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agitq.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "4.13"
+        versionCode = 15
+        versionName = "4.14"
     }
 
     signingConfigs {
