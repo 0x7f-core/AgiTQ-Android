acb7ce801c9523e2fc83b057080bb822cdfcbc2a
0x7f-core <104748634+0x7f-core@users.noreply.github.com>
2026-09-01 11:13:27 +0900

    Bump Android widget build to version 4.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agitq.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "4.2"
+        versionCode = 4
+        versionName = "4.3"
     }
 
     buildTypes {
